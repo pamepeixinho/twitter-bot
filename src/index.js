f1ba9a330f6e5522ff800d1bb06fba3a03b2d939
Pamela Peixinho <pamela.peixinho@quintoandar.com.br>
2018-04-22 22:19:36 -0300

    Extract twitter secret keys to EnvVar (#2)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const Twitter = require('twitter');
 
+require('dotenv').config();
+
 const client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
 const stream = client.stream('statuses/filter', { track:'#LittleFishBot' });
